f4ae9228419f570818092c4f1f5ab72c65274c16
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-04-06 02:07:07 -0500

    Primera parte del conversor de moneda, pasando de pesos colombianos a dolar

---
+++ Basico/conversor_dolar.py
@@ -0,0 +1,5 @@
+pesos = float(input("Por favor ingrese el valor en pesos colombianos que desea convertir: "))
+valor_dolar = 3875
+dolares_co = pesos / valor_dolar
+
+print(f"Es equivalente a {round(dolares_co,2)} dolares")
